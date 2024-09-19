9f53251930af0283e81c1c75d7d2a0550c9211c9
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-19 16:17:09 +0300

    sidebar - added user button and details

--- app/(dashboard)/_components/MemberProfile.jsx
+++ app/(dashboard)/_components/MemberProfile.jsx
@@ -1,6 +1,19 @@
-const MemberProfile = () => {
+import { UserButton } from "@clerk/nextjs";
+import { currentUser, auth } from "@clerk/nextjs/server";
+
+const MemberProfile = async () => {
+  const user = await currentUser();
+
   return (
-    <div>MemberProfile</div>
-  )
-}
-export default MemberProfile
+    <div className="px-4 flex items-center gap-4">
+      <UserButton />
+      <div className="join join-vertical">
+        <p className="text-2xl join-item">
+          {user.firstName} {user.lastName}
+        </p>
+        <p className="text-sm join-item">{user.emailAddresses[0].emailAddress}</p>
+      </div>
+    </div>
+  );
+};
+export default MemberProfile;
