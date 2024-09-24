e9015b85f0d81f14af2a28575d7c4bc2e96192a8
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-24 08:43:50 +0300

    added user new tour input

--- app/(dashboard)/tours/new-tour/_components/NewTour.jsx
+++ app/(dashboard)/tours/new-tour/_components/NewTour.jsx
@@ -1,4 +1,31 @@
+"use client";
+
+import TourInfo from "./TourInfo";
+
 const NewTour = () => {
-  return <div>NewTour</div>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    console.log(destination);
+  };
+
+  return (
+    <>
+      <form className="max-w-2xl" onSubmit={handleSubmit}>
+        <h2 className="mb-4">Select your dream destination:</h2>
+        <div className="join w-full">
+          <input className="input input-bordered join-item w-full" placeholder="country" name="country" type="text" required />
+          <input className="input input-bordered join-item w-full" placeholder="city" name="city" type="text" required />
+          <button className="btn btn-primary join-item" type="submit">
+            Generate tour
+          </button>
+        </div>
+      </form>
+      <div className="mt-16">
+        <TourInfo />
+      </div>
+    </>
+  );
 };
 export default NewTour;
